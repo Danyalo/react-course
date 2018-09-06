7a51f5065b1398a743c2d2fa1e4c5b2b3cd2b6b1
Danylo Labenskyi <danylo.labenskyi@jetsoftpro.com>
2018-09-06 09:35:53 +0300

    Finish Module 4 - Lists

--- src/App.js
+++ src/App.js
@@ -5,31 +5,33 @@ import Person from "./Person/Person";
 class App extends Component {
   state = {
     persons: [
-      { name: "Max", age: 28 },
-      { name: "Manu", age: 29 },
-      { name: "Lara", age: 23 }
+      { id: "sadf", name: "Max", age: 28 },
+      { id: "fdsf", name: "Manu", age: 29 },
+      { id: "aaaa", name: "Lara", age: 23 }
     ],
     showPersons: false
   };
 
-  switchNameHandler = newName => {
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = { ...this.state.persons[personIndex] };
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
     this.setState({
-      persons: [
-        { name: newName, age: 28 },
-        { name: "Manu", age: 29 },
-        { name: "Lara", age: 115 }
-      ]
+      persons: persons
     });
   };
 
-  nameChangedHandler = event => {
-    this.setState({
-      persons: [
-        { name: "Max", age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: "Lara", age: 115 }
-      ]
-    });
+  deletePersonHandler = personIndex => {
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons });
   };
 
   togglePersonsHandler = () => {
@@ -50,8 +52,16 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          {this.state.persons.map(person => {
-            return <Person name={person.name} age={person.age} />;
+          {this.state.persons.map((person, index) => {
+            return (
+              <Person
+                key={person.id}
+                name={person.name}
+                age={person.age}
+                changed={event => this.nameChangedHandler(event, person.id)}
+                click={() => this.deletePersonHandler(index)}
+              />
+            );
           })}
         </div>
       );
